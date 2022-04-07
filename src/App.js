6020231b0ea4362acba3de2974e0087958c49b07
lovelynoel <shduf123@naver.com>
2022-04-07 13:18:23 +0900

    rating 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import Fruit from './Orange'; //앞이 컴포먼트 이름 뒤에가 파일이름
 // 컴포 불러오기
+
+//map 함수 사용해서 불러오기
+const Fruitlike=[
+  {
+    id: 1,
+    name : 'banana',
+    image : 'https://cdn-icons-png.flaticon.com/512/284/284780.png',
+    rating : 5,
+  },
+  {
+    id: 2,
+    name : 'apple',
+    image : 'https://cdn-icons-png.flaticon.com/512/4583/4583405.png',
+    rating : 4.9,
+  },
+  {
+    id: 3,
+    name : 'orange',
+    image : 'https://cdn-icons-png.flaticon.com/512/135/135620.png',
+    rating : 4.7,
+  }
+]
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-        <Fruit fav='banana'/>
-        <Fruit fav='apple'/>
-        <Fruit fav='orange'/>
+      {Fruitlike.map((dish) => <Fruit key={dish.id} name={dish.name} picture={dish.image}/>)}
     </div>
   );
 }
--- src/Orange.js
+++ src/Orange.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
-/* function Fruit(props){ // 대문자로 시작
+/* function Fruit(props){ // 대문자로 시작 방법1
     console.log(props)
     return(
         <h3>I love {props.fav}</h3> //꼭 return 값 안에 작성하기
     )
 } */
 
-function Fruit( {fav} ){
-    return <h1> I like {fav} </h1>
-}
+function Fruit( {name, picture} ){ 
+    return (
+        <div>
+            <h1> I like {name} </h1>
+            <img src={picture} width="300" alt={name} />
+        </div>
+    )
+} // 방법2
+
+
 
 export default Fruit;
